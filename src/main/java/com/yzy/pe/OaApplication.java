6a70c2e11213457baf168f551f3485339de9a4fb
Berserker <dddz97@163.com>
2019-03-22 17:21:29 +0800

    init

--- src/main/java/com/yzy/pe/OaApplication.java
+++ src/main/java/com/yzy/pe/OaApplication.java
@@ -16,7 +16,7 @@ public class OaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OaApplication.class, args);
-        System.out.println("springboot start.");
+        System.out.println("springboot started!");
     }
 
 }

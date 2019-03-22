8cfcdac594487c08b376f9e935611d173d28899f
Berserker <dddz97@163.com>
2019-03-22 14:13:48 +0800

    init

--- src/main/java/com/zjw/oa/controller/UserController.java
+++ src/main/java/com/zjw/oa/controller/UserController.java
@@ -3,7 +3,6 @@ package com.zjw.oa.controller;
 import com.zjw.oa.entity.User;
 import com.zjw.oa.service.UserService;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,23 +28,18 @@ public class UserController {
     /**
      * Description 登录控制器，前后端分离用的不同协议和端口，可以加入@CrossOrigin支持跨域。
      *
-     * @param user          user
-     * @param bindingResult 在参数中加入BindingResult来获取错误信息，判断BindingResult知否含有错误信息，如果有错误信息，则直接返回错误信息。
+     * @param user user
      * @return Result
      * @author ZhengJiawei
      * @date 2019-03-21 09:06:08
      */
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public ModelAndView login(User user, BindingResult bindingResult) {
+    public ModelAndView login(User user) {
         ModelAndView mav = new ModelAndView("login");
-        if (bindingResult.hasErrors()) {
-            return mav;
-        }
 
         User user1 = userService.login(user);
         if (user1 == null) {
-            mav.setViewName("login");
             return mav;
         }
         mav.setViewName("index");

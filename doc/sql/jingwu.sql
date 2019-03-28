15b02c6391454c235cf325bcfd8294f19b724058
Berserker <dddz97@163.com>
2019-03-28 13:40:47 +0800

    sql

--- doc/sql/jingwu.sql
+++ doc/sql/jingwu.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 21/03/2019 15:18:00
+ Date: 28/03/2019 13:40:04
 */
 
 SET NAMES utf8mb4;
@@ -36,6 +36,24 @@ CREATE TABLE `add_point`  (
 INSERT INTO `add_point` VALUES (1, '1', 'zjwzjw', '见义勇为', 10);
 
 -- ----------------------------
+-- Table structure for advice
+-- ----------------------------
+DROP TABLE IF EXISTS `advice`;
+CREATE TABLE `advice`  (
+  `adviceId` int(4) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '建议题目',
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '建议内容',
+  `isCross` int(1) NULL DEFAULT NULL COMMENT '1：已通过，2：未通过',
+  `orderTime` datetime(0) NULL DEFAULT NULL COMMENT '提交时间',
+  PRIMARY KEY (`adviceId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of advice
+-- ----------------------------
+INSERT INTO `advice` VALUES (1, '建议一', '放假', 1, '2019-03-28 11:30:54');
+
+-- ----------------------------
 -- Table structure for delete_point
 -- ----------------------------
 DROP TABLE IF EXISTS `delete_point`;
@@ -46,7 +64,7 @@ CREATE TABLE `delete_point`  (
   `deleteName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '扣分项目名称',
   `deletePoint` int(4) NULL DEFAULT NULL COMMENT '扣除分数',
   PRIMARY KEY (`deleteId`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of delete_point
@@ -58,11 +76,17 @@ INSERT INTO `delete_point` VALUES (1, '1', 'zjwzjw', '被子不整齐', 2);
 -- ----------------------------
 DROP TABLE IF EXISTS `permission`;
 CREATE TABLE `permission`  (
-  `permisssionId` int(4) NOT NULL AUTO_INCREMENT,
+  `permissionId` int(4) NOT NULL AUTO_INCREMENT,
   `permissionName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `permissionDegree` int(4) NULL DEFAULT NULL,
-  PRIMARY KEY (`permisssionId`) USING BTREE
+  `perUrl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`permissionId`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of permission
+-- ----------------------------
+INSERT INTO `permission` VALUES (1, '管理员', 1, '/user/adminMsg');
 
 -- ----------------------------
 -- Table structure for punish
@@ -76,7 +100,7 @@ CREATE TABLE `punish`  (
   `punishReason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '惩罚原因',
   `punishComplete` int(1) NULL DEFAULT NULL COMMENT '惩罚是否完成 1：完成 2：未完成',
   PRIMARY KEY (`punishId`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of punish
@@ -112,12 +136,12 @@ CREATE TABLE `user`  (
   `qdbm` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '区队编码',
   `kpfs` double(4, 0) NULL DEFAULT NULL COMMENT '考评分数',
   PRIMARY KEY (`userId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'zjwzjw', '123456', 3, 'zjjc1591801', NULL);
+INSERT INTO `user` VALUES ('15918135', 'zjwzjw', '123456', 1, 'zjjc1591801', 118);
 
 -- ----------------------------
 -- Table structure for user_wj
@@ -134,6 +158,11 @@ CREATE TABLE `user_wj`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of user_wj
+-- ----------------------------
+INSERT INTO `user_wj` VALUES (1, '电器', '寝室火锅', 'zjwzjw', '15918135', 10);
+
+-- ----------------------------
 -- Table structure for weak_check
 -- ----------------------------
 DROP TABLE IF EXISTS `weak_check`;
@@ -144,5 +173,10 @@ CREATE TABLE `weak_check`  (
   `checkPoint` int(4) NULL DEFAULT NULL COMMENT '检查分数',
   PRIMARY KEY (`checkId`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of weak_check
+-- ----------------------------
+INSERT INTO `weak_check` VALUES (1, '卫生', '寝室卫生', 10);
 
 SET FOREIGN_KEY_CHECKS = 1;

5a8010339c195c08897425058cf528aec72111f8
dddz <dddz97@163.com>
2019-04-14 08:49:55 +0800

    page2

---
+++ src/main/java/com/yzy/pe/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,13 @@
+package com.yzy.pe.service.impl;
+
+import com.yzy.pe.service.TeacherService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author YanZiyi
+ * @create 2019-03-29 9:50
+ */
+@Service
+public class TeacherServiceImpl implements TeacherService {
+
+}

5f7be3fcd91429f606b911d3cdff69cdec5a8194
Berserker <dddz97@163.com>
2019-04-14 18:09:02 +0800

    page

---
+++ src/main/java/com/yzy/pe/controller/AdminController.java
@@ -0,0 +1,25 @@
+package com.yzy.pe.controller;
+
+import com.yzy.pe.service.UserService;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Description user
+ *
+ * @author YanZiyi
+ * @date 2019-03-21 10:49:24
+ */
+@RestController
+@EnableAutoConfiguration
+@RequestMapping(value = "/user")
+public class AdminController {
+
+    @Resource
+    private UserService userService;
+
+
+}
